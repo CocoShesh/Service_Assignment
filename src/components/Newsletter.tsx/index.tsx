41cea0a9cabcdbbb366b5d040ca3dd7b33b37dd4
NARD <glenardrefamonte1@gmail.com>
2024-08-12 19:11:39 +0800

    Updated

--- src/components/Newsletter.tsx/index.tsx
+++ src/components/Newsletter.tsx/index.tsx
@@ -3,32 +3,34 @@ const Newsletter = () => {
   return (
     <>
       <section className="h-[550px] mb-20 w-full   flex  items-center justify-center max-md:flex-col gap-10 px-14  py-10  max-md:px-0 max-lg:h-fit bg-[#888e93] text-white ">
-        <section className="w-full  2xl:w-[900px]">
-          <img
-            src="./images/newsletter.png"
-            alt=""
-            className=" object-contain"
-          />
-        </section>
-        <section className="w-full 2xl:w-full gap-5 flex 2xl:px-32 flex-col items-center justify-center pl-20 max-md:px-5 max-md:w-full">
-          <h1 className="text-left text-4xl font-bold">
-            Subscribe to our Newsletter!
-          </h1>
-          <p>
-            Be the first to get exclusive offers by subscribing to our
-            newsletter.
-          </p>
-          <section className="relative w-full ">
-            <HiOutlineMail className="absolute top-4 left-2 text-2xl text-black " />
-            <input
-              type="email"
-              className="w-full h-14 border  pl-10 "
-              placeholder="Enter your email address"
+        <section className="2xl:w-[1700px] flex ">
+          <section className="w-full  2xl:w-[1200px]">
+            <img
+              src="./images/newsletter.png"
+              alt=""
+              className=" object-contain"
             />
           </section>
-          <button className="w-full h-14  bg-black text-white ">
-            Subcribe Now
-          </button>
+          <section className="w-full 2xl:w-full gap-5 flex 2xl:px-32 flex-col items-center justify-center pl-20 max-md:px-5 max-md:w-full">
+            <h1 className="text-left text-4xl font-bold">
+              Subscribe to our Newsletter!
+            </h1>
+            <p>
+              Be the first to get exclusive offers by subscribing to our
+              newsletter.
+            </p>
+            <section className="relative w-full ">
+              <HiOutlineMail className="absolute top-4 left-2 text-2xl text-black " />
+              <input
+                type="email"
+                className="w-full h-14 border  pl-10 "
+                placeholder="Enter your email address"
+              />
+            </section>
+            <button className="w-full h-14  bg-black text-white ">
+              Subcribe Now
+            </button>
+          </section>
         </section>
       </section>
     </>
