283c2f38234b5f41432bff77852e6a6f9e4ce85e
NARD <glenardrefamonte1@gmail.com>
2024-08-12 20:13:52 +0800

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       backgroundImage: {
         plan: "url('./images/plan-bg-section.webp')",
         selling: "url('./images/Selling-bg.webp')",
-        estate: "url('/images/real-estate2.png.')",
+        estate: "url('/images/real-estate2.png')",
         contact: "url('./images/contact-bg.png')",
       },
       screens: {
